6e1b6d5478d27fdb718f42cbecd1a44ddd23d4e9
Julia Thomias <julia.thomias@gmail.com>
2014-07-02 14:06:59 +0200

    gtre

---
+++ function_pointer/pointer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+typedef int (*pf1_t) (int, int);
+
+int pointer(int a, int b) {
+	printf("a=%d and b=%d\n", a, b);
+	return 0;
+}
+
+int truc(int a, int b) {
+	printf("truc: a=%d and b=%d\n", a, b);
+	return 0;
+}
+
+int hello(pf1_t a, pf1_t b) {
+	a(3, 4);
+	b(2, 5);
+	return 0;
+}
+
+int main() {
+	pf1_t pf1;
+	pf1 = &truc;
+	pf1(2, 3);
+	hello(truc, pointer);
+	return 0;
+}
