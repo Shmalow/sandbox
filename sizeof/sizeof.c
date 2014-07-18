3eb05ca3a84e436db4ef59a892aa414eac65336b
Julia Thomias <julia.thomias@gmail.com>
2014-07-18 16:03:56 +0200

    gre

---
+++ sizeof/sizeof.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	printf("Size of char: %d\n", sizeof(char));
+	printf("Size of short int: %d\n", sizeof(short int));
+	printf("Size of int: %d\n", sizeof(int));
+	printf("Size of long int: %d\n", sizeof(long int));
+	printf("Size of long long int: %d\n", sizeof(long long int));
+	printf("Size of double: %d\n", sizeof(double));
+	
+	return 0;
+}
